efa4ec67ad64b88edcbad489aec2d6a5dcfc87d3
Akash6222 <akashdubey25274@gmail.com>
2024-04-03 16:51:53 +0530

    upadted light_node commands

--- avail_light_node_commands.sh
+++ avail_light_node_commands.sh
@@ -24,11 +24,15 @@ WantedBy=multi-user.target
 EOF
 
 sudo systemctl daemon-reload
-#sudo systemctl enable availightd
+sudo systemctl enable availightd
 sudo systemctl start availightd
 # sudo systemctl status availightd
 
 timeout 5s  journalctl -f -u availightd.service --no-hostname -o cat | sed 's/\x1b\[[0-9;]*m//g' > avail_light.log
-grep "public key" availightd.log | awk '{print $NF}' > public_key.txt
+grep "public key" avail_light.log | awk '{print $NF}' > public_key.txt
 cat public_key.txt
 #curl -sL1 http://avail.sh | bash
+
+
+
+
